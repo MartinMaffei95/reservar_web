0c9ce4c7c192a3386e8e11d4d3f25bb1aae3ae4b
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2022-10-02 02:04:50 -0300

    Creado el proyecto inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const getData = () => {
+    fetch('http://localhost:5000/buildings/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYzMzYxMTExYzcwMDExNDQxN2QzMDg0OSIsInVzZXJuYW1lIjoibWFydGluIiwicGFzc3dvcmQiOiIkMmIkMTAkQ25xaFFVNEVkZHlHeFEwc0hzMUp3T0xoWDY4OHA1OC9pUXpkV0RXem43cTBYWk1nc0hua2kiLCJjcmVhdGVkQXQiOjE2NjQ0ODc2OTc2NzMsInVwZGF0ZWRBdCI6MTY2NDQ4NzY5NzY3MywiX192IjowfSwiaWF0IjoxNjY0NTA0MDAyfQ.famG5lYqHDKfe-xxJcyL9EslkBJkXbBle82_y-1MZ60',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
